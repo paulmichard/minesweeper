019e83e06d3cf99daed252268f3e575794512490
Paul Michard <paulmichard@hotmail.com>
2020-05-30 19:56:39 -0300

    Setup Swagger for API's

---
+++ src/main/java/com/paulmichard/minesweeper/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.paulmichard.minesweeper;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.paulmichard.minesweeper.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Minesweeper",
+				"Deviget Challenge - Minesweeper Backend Service",
+				"API v1",
+				null,
+				new Contact("Paul Michard","https://github.com/paulmichard", "paulmichard@hotmail.com"),
+				"MIT",
+				null,
+				Collections.emptyList());
+	}
+}
--- src/main/java/com/paulmichard/minesweeper/controller/GameController.java
+++ src/main/java/com/paulmichard/minesweeper/controller/GameController.java
@@ -13,28 +13,84 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.paulmichard.minesweeper.bean.GameBoardBean;
 import com.paulmichard.minesweeper.bean.GameRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RequestMapping(value = "/minesweeper/api/v1/games")
+@Api(value = "Game Controller")
 public interface GameController {
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "POST",
+			value = "Creates a new game")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Game created", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Game not found"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> createNewGame(@Valid @RequestBody GameRequest gameRequest);
 
 	@GetMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "GET",
+			value = "Loads an existing game")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Game loaded", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Game not found"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> loadGame(@PathVariable Long id);
 
 	@PatchMapping(path = "/{id}/pause", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "PATCH",
+			value = "Pauses a game that is still active")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Game paused", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Game not found"),
+			@ApiResponse(code = 409, message = "Game is not active"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> pauseGame(@PathVariable Long id);
 
 	@PatchMapping(path = "/{id}/resume", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "PATCH",
+			value = "Resumes a game that is still active")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Game resumed", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Game not found"),
+			@ApiResponse(code = 409, message = "Game is not active"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> resumeGame(@PathVariable Long id);
 
 	@PostMapping(path = "/{id}/cells/{cellId}/flag", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "POST",
+			value = "Flags a cell")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Cell flagged", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Cell not found"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> flagCell(@PathVariable Long id, @PathVariable Long cellId);
 
 	@PostMapping(path = "/{id}/cells/{cellId}/questionMark", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "POST",
+			value = "Adds a question mark to a cell")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Cell marked", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Cell not found"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> questionMarkCell(@PathVariable Long id, @PathVariable Long cellId);
 
 	@PostMapping(path = "/{id}/cells/{cellId}/show", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(
+			httpMethod = "POST",
+			value = "Reveals the cell")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Cell flagged", response = GameBoardBean.class),
+			@ApiResponse(code = 404, message = "Cell not found"),
+			@ApiResponse(code = 500, message = "Unknown error")})
 	ResponseEntity<GameBoardBean> showCell(@PathVariable Long id, @PathVariable Long cellId);
 }
